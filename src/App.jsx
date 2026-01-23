d11a1cd8bc1fce9d6ad80c2c5c07b5cf2ea20b0b
noblesse <cnsamy02@gmail.com>
2026-01-23 10:00:17 +0100

    modifie les modules du site au norme du bts, regarder les doc bts

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Skill />
       <Parcours />
       <Projet />
+      <Skill />
       <Veille />
       <Contact />
       <Footer />
